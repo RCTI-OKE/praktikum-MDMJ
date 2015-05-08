58d9358930a6e9494a42180a07b00d01ea3c1eeb
SBC4 <SBC4@SBC08>
2015-05-08 17:17:23 +0700

    uda tak kasih pembagian

---
+++ src/rcti/oke/pembagian_fix.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rcti.oke;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SBC4
+ */
+public class pembagian_fix {
+     public static void main(String[] args) {
+        Scanner masuk=new Scanner(System.in);
+System.out.print("NILAI PERTAMA:");
+int N1= masuk.nextInt();
+
+System.out.print("NILAI KEDUA:");
+int N2= masuk.nextInt();
+
+int hasil=N1/N2;
+System.out.print ("hasil total=" +hasil);
+    }
+}
+
+
